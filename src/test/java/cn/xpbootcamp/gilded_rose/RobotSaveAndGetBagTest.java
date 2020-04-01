2009a9b7786b06b4308f076c7204cd5a72b8df1f
yu.zhu1 <yu.zhu1@thoughtworks.com>
2020-04-01 22:17:56 +0800

    feat: should get error when robot save nothing

--- src/test/java/cn/xpbootcamp/gilded_rose/RobotSaveAndGetBagTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/RobotSaveAndGetBagTest.java
@@ -13,4 +13,14 @@ public class RobotSaveAndGetBagTest {
         Ticket ticket = lockerRobot.saveBag(bag);
         assertNotNull(ticket);
     }
+
+    @Test
+    void should_get_error_message_when_the_robot_help_to_save_nothing_into_locker() {
+        Cabinet cabinet = createCabinetWithPlentyOfCapacity();
+        LockerRobot lockerRobot = new LockerRobot(cabinet);
+        String message = assertThrows(IllegalArgumentException.class, () -> {
+            lockerRobot.saveBag(null);
+        }).getMessage();
+        assertEquals("Please put a bag into the cabinet.", message);
+    }
 }
