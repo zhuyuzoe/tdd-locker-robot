2d0679db3de7231bed659b67364208cc480fba5f
yu.zhu1 <yu.zhu1@thoughtworks.com>
2020-04-09 18:27:43 +0800

    feat: get error when give invalid ticket to robot after save bag to cabinet with two lockers successfully

--- src/test/java/cn/xpbootcamp/gilded_rose/SmartRobotSaveAndGetBagTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/SmartRobotSaveAndGetBagTest.java
@@ -134,4 +134,22 @@ public class SmartRobotSaveAndGetBagTest {
         }).getMessage();
         assertEquals("Please insert a ticket to get your bag.", message);
     }
+
+    @Test
+    void should_get_invalid_ticket_error_when_robot_save_bag_into_cabinet_with_first_locker_of_3_capacity_left_and_second_of_2_successfully_and_give_robot_invalid_ticket() {
+        // Given
+        Cabinet cabinet = createCabinetWithLockersOfCapacityLeft(new ArrayList<>(Arrays.asList(3, 2)));
+        Bag savedBag = new Bag();
+        LockerRobot lockerRobot = new LockerRobot(cabinet);
+
+        // When
+        lockerRobot.saveBag(savedBag);
+
+        // Then
+        Ticket invalidTicket = new Ticket();
+        String message = assertThrows(InvalidTicketException.class, () -> {
+            lockerRobot.getBag(invalidTicket);
+        }).getMessage();
+        assertEquals("Please insert a valid ticket.", message);
+    }
 }
