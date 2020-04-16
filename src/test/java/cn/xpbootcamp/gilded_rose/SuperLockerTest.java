0ddc515b9997e0bd03deb702c8ae9f6d41675e15
yu.zhu1 <yu.zhu1@thoughtworks.com>
2020-04-17 01:59:04 +0800

    refactor: rename the private field

--- src/test/java/cn/xpbootcamp/gilded_rose/SuperLockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/SuperLockerTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SuperLockerTest {
-    private final int CABINET_DEFAULT_CAPACITY = 10;
+    private final int DEFAULT_CAPACITY = 10;
 
     @Test
     void should_get_the_ticket_when_super_robot_save_bag_into_one_locker_with_enough_capacity() {
@@ -33,7 +33,7 @@ public class SuperLockerTest {
     @Test
     void should_throw_insufficient_locker_exception_when_super_robot_save_bag_into_one_locker_which_is_full() {
         List<Locker> lockers = new ArrayList<>();
-        lockers.add(createFullLocker(CABINET_DEFAULT_CAPACITY));
+        lockers.add(createFullLocker(DEFAULT_CAPACITY));
         Bag bag = new Bag();
         SuperLockerRobot superLockerRobot = new SuperLockerRobot(lockers);
         assertThrows(InsufficientLockersException.class,
