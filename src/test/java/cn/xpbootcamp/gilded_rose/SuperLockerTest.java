e76b404a458c88ceb7f9b5c431f9f9f02ae039ee
yu.zhu1 <yu.zhu1@thoughtworks.com>
2020-04-17 00:47:34 +0800

    feat: should throw if super robot save bag to one full locker

--- src/test/java/cn/xpbootcamp/gilded_rose/SuperLockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/SuperLockerTest.java
@@ -1,20 +1,37 @@
 package cn.xpbootcamp.gilded_rose;
 
+import cn.xpbootcamp.gilded_rose.exception.InsufficientLockersException;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static cn.xpbootcamp.gilded_rose.LockersFactory.createFullLocker;
 import static cn.xpbootcamp.gilded_rose.LockersFactory.createLockersOfPlentyOfCapacity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SuperLockerTest {
+    private final int CABINET_DEFAULT_CAPACITY = 10;
 
     @Test
-    void should_get_the_ticket_when_super_robot_save_bag_into_cabinet_with_one_locker_of_enough_capacity() {
+    void should_get_the_ticket_when_super_robot_save_bag_into_one_locker_with_enough_capacity() {
         List<Locker> lockersOfPlentyOfCapacity = createLockersOfPlentyOfCapacity(1);
         Bag bag = new Bag();
         SuperLockerRobot superLockerRobot = new SuperLockerRobot(lockersOfPlentyOfCapacity);
         Ticket ticket = superLockerRobot.saveBag(bag);
         assertNotNull(ticket);
     }
+
+    @Test
+    void should_throw_insufficient_locker_exception_when_super_robot_save_bag_into_one_locker_which_is_full() {
+        List<Locker> lockers = new ArrayList<>();
+        lockers.add(createFullLocker(CABINET_DEFAULT_CAPACITY));
+        Bag bag = new Bag();
+        SuperLockerRobot superLockerRobot = new SuperLockerRobot(lockers);
+        assertThrows(InsufficientLockersException.class,
+                () ->  superLockerRobot.saveBag(bag));
+    }
+
 }
